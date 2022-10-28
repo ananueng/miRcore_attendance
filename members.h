b4d6c2891eef8f4330c5236d31c902ce5d5d735c
ananueng <ananueng@umich.edu>
2022-10-28 15:42:18 -0400

    First draft, need to debug

--- members.h
+++ members.h
@@ -15,28 +15,71 @@ using namespace std;
 
 struct zoomAlias {
     string alias;
-    uint32_t time;
+    string time;
 };
 
 struct member {
     member() {}
-    member(const string &first, const string &last) {
+    member(const string &output, const string &full, const string &first, const string &last) {
+        outName = output;
+        fullName = full;
         firstName = first;
         lastName = last;
     }
-    vector<zoomAlias> zoomAliases;
+    deque<zoomAlias> zoomAliases;
+    string outName;
+    string fullName;
     string firstName;
     string lastName;
+    bool fullMatch = false;
+    bool partialMatch = false;
+    bool filler = false;
 };
 
 class Members {
     private:
         vector<member> attendanceList;
         vector<member*> sortedAttendanceList;
+        vector<string> notMatched;
         string attendanceListFile;
         string zoomLogFile;
         string date;
         bool isHelp = false;
+        bool find(string full, string &time) {
+            bool foundMatch = false;
+            string fullOnlyLetters;
+            for (int j = 0; j < full.length(); j++) {
+                if ((int(full[j]) >= 'a' && int(full[j]) <= 'z') || full[j] == ' ') {
+                    fullOnlyLetters.push_back(full[j]);
+                }
+            }
+            full = fullOnlyLetters;
+            for (size_t i = 0; i < attendanceList.size(); i++) {
+                size_t foundFirst = full.find(attendanceList[i].firstName);
+                size_t foundLast = full.find(attendanceList[i].lastName);
+                if (full == attendanceList[i].fullName || 
+                    (foundFirst != std::string::npos && foundLast != std::string::npos)) {
+                    zoomAlias toPush = {full, time};
+                    attendanceList[i].zoomAliases.push_front(toPush);
+                    attendanceList[i].fullMatch = true;
+                    foundMatch = true;
+                }
+                else {
+                    string temp;
+                    stringstream ss(full);
+                    while (getline(ss, temp, ' ')) {
+                        if (temp == attendanceList[i].lastName || 
+                            temp == attendanceList[i].firstName) {
+                        zoomAlias toPush = {full, time};
+                        attendanceList[i].zoomAliases.push_back(toPush);
+                        attendanceList[i].partialMatch = true;   
+                        foundMatch = true; 
+                        }
+                    }
+                } 
+            }
+            return foundMatch;
+        }
     public:
         class firstNameLess {
             public: 
@@ -105,58 +148,54 @@ class Members {
 
         bool getHelp() {return isHelp;}
 
-        template<typename COMPARE = firstNameLess>
+        //template<typename COMPARE = firstNameLess>
         void readAttendanceList() {
             string junk;
             csvstream csvin(attendanceListFile);
-            COMPARE less;
+            //COMPARE less;
 
             map<string, string> row;
             csvin >> row;
             while (csvin >> row) {
-                if (row["Name (First Last)"] != "") {
+                string full = row["Name (First Last)"];
+                transform(full.begin(), full.end(), full.begin(), ::tolower);
+                if (full != "") {
                     string first;
                     string last;
                     uint32_t i = 0;
 
                     //read firstName until space or comma
-                    while (row["Name (First Last)"][i] != ' ' && 
-                        row["Name (First Last)"][i] != ',' && 
-                        i < row["Name (First Last)"].size()) {
-
-                        first.push_back(row["Name (First Last)"][i++]);
+                    while (full[i] != ' ' && full[i] != ',' && i < full.size()) {
+                        first.push_back(full[i++]);
                     }
 
                     //skip the space or comma in between
-                    while ((row["Name (First Last)"][i] == ' ' || 
-                            row["Name (First Last)"][i] == ',') &&
-                            i < row["Name (First Last)"].size()) {
-
+                    while ((full[i] == ' ' || full[i] == ',') && i < full.size()) {
                         i++;
                     }
 
                     //read lastName until space or end
-                    while (row["Name (First Last)"][i] == ' ' &&
-                           i < row["Name (First Last)"].size()) {
-
-                        last.push_back(row["Name (First Last)"][i++]);
+                    while (full[i] != ' ' && i < full.size()) {
+                        last.push_back(full[i++]);
                     }
 
                     //add name (all lowercase) to master list
-                    transform(first.begin(), first.end(), first.begin(), ::tolower);
-                    transform(last.begin(), last.end(), last.begin(), ::tolower);
-                    attendanceList.push_back(member(first, last));
+                    attendanceList.push_back(member(row["Name (First Last)"], full, first, last));
+                }
+                else { //pushback empty for formatting
+                    attendanceList.push_back(member());
                 }
             }
 
             //create sorted attendance list for faster search
-            for (auto &i:attendanceList) {
-                sortedAttendanceList.push_back(&i);
-            }
-            sort(sortedAttendanceList.begin(), sortedAttendanceList.end(), less);
+            // for (auto &i:attendanceList) {
+            //     sortedAttendanceList.push_back(&i);
+            // }
+            // sort(sortedAttendanceList.begin(), sortedAttendanceList.end(), less);
         }
-
+        // template<typename COMPARE = firstNameLess>
         void readZoomLogs() {
+            //COMPARE less;
             string junk;
             csvstream csvin(zoomLogFile);
 
@@ -166,15 +205,24 @@ class Members {
             date = dateRow["Start Time"];
 
             //read the rest
-            csvin.set_new_header(); // empty line
+            csvin.set_new_header(); // empty line in zoomLogs
             csvin.set_new_header(); // first row (new header) of attendance
+
             map<string, string> attendanceRow;
             while (csvin >> attendanceRow) {
+                string temp = attendanceRow["Name (Original Name)"];
+                string time = attendanceRow["Total Duration (Minutes)"];
+                //vector<member>::iterator it;
+                transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+                if (!find(temp, time)) {
+                    notMatched.push_back(attendanceRow["Name (Original Name)"]);
+                };
                 //bin search (lower case) in sorted attendance list, add alias and time
+                // auto it = attendanceList;
+                // it = find(attendanceList.begin(), attendanceList.end(), temp);
                 //date = attendanceRow["Start Time"];
             }
         }
-
         void printSummary() {
             //print only the date, not the time
             uint32_t i = 0;
@@ -184,7 +232,36 @@ class Members {
             cout << endl;
 
             //print the attendance
-
+            for (size_t i = 0; i < attendanceList.size(); i++) {
+                cout << attendanceList[i].outName << ": ";
+                if (attendanceList[i].fullMatch) {
+                    //only cout the first zoomAlias, which will be the full name
+                    cout << attendanceList[i].zoomAliases.front().alias << "|" <<
+                    attendanceList[i].zoomAliases.front().time << "|,";
+                }
+                else if (attendanceList[i].partialMatch) {//partial match
+                    for (size_t j = 0; j < attendanceList[i].zoomAliases.size(); i++) {
+                    cout << attendanceList[i].zoomAliases[j].alias << "|" <<
+                    attendanceList[i].zoomAliases[j].time << "|, ";
+                    }
+                }
+                else {
+                    if (attendanceList[i].filler) {
+                        cout << "this is filler";
+                    }
+                    else {
+                        cout << "ABSENT";
+                    }
+                }
+                cout << endl;
+            }
+        //print unmatched
+            if (!notMatched.empty()) {
+                cout << "Names in zoomLogs that weren't matched: ";
+                for (auto &i: notMatched){
+                    cout << i << endl;
+                }
+            }
         }
 };
 
